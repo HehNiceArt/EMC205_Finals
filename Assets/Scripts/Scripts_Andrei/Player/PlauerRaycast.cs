80221ec5e1bc4b9d1afb8914d89422abae17064c
CIIT502PC03\Student <it@ciit.edu.ph>
2024-02-17 15:38:04 +0800

    seifer lab u

---
+++ Assets/Scripts/Scripts_Andrei/Player/PlauerRaycast.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlauerRaycast : MonoBehaviour
+{
+    [SerializeField] private float _raycastDistance;
+    // layer mask only contains tree layer
+    [SerializeField] private LayerMask _layerMask;
+
+    Camera _cam;
+
+    void Start()
+    {
+        _cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(_cam.transform.position, _cam.transform.forward, out RaycastHit hit, _raycastDistance, _layerMask))
+        {
+            if (hit.collider.CompareTag("Tree") && Input.GetKey(KeyCode.E))
+            { 
+                TreeGrow._Instance.GrowTreeScale();
+            }
+        }
+    }
+}
