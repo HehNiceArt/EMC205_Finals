30f8afec978d8898e12fac40cffd43687cffbc2b
sandsocks <max.sandejas@gmail.com>
2024-02-25 21:07:57 +0800

    fixed main menu layout and added photo placeholder for background (will replace once we start working on aesthetics)

--- Assets/Scripts/Scripts_Maxi/MainMenu.cs
+++ Assets/Scripts/Scripts_Maxi/MainMenu.cs
@@ -5,13 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public CanvasGroup SettingsPanel;
+    public CanvasGroup CreditsPanel;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("MASTER_MainLevel");
     }
 
-    public void QuitGame()
+    public void Option()
     {
-        Application.Quit();
+        SettingsPanel.alpha = 1;
+        SettingsPanel.blockRaycast = true;
+    }
+
+    public void Credits()
+    {
+        
+    }
+
+    public void Back()
+    {
+        SettingsPanel.alpha = 0;
+        SettingsPanel.blockRaycast = false;
+    }
+
+    public void Quit()
+    {
+        
     }
 }
