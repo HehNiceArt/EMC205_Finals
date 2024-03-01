5e56928d97176704db5e08bb2184df71f7a18367
Andrei Quirante <andrei.quirante@ciit.edu.ph>
2024-03-02 01:07:51 +0800

    Flower, Leaf, and Matrix4x4
    Added flowers and leaf in the tree instancing
    Implemented a Matrix4x4 rotations for the pitch, roll, and turns

--- Assets/Scripts/Scripts_Andrei/Procedural Tree/TreeLSystem3D.cs
+++ Assets/Scripts/Scripts_Andrei/Procedural Tree/TreeLSystem3D.cs
@@ -8,6 +8,8 @@ using UnityEditorInternal;
 using UnityEngine.ProBuilder;
 using System.Linq;
 using UnityEngine.Rendering.Universal;
+using TMPro;
+using TreeEditor;
 
 public class TransformInfo3D
 {
@@ -20,19 +22,19 @@ public class TreeLSystem3D : MonoBehaviour
 
     [Header("Tree Values")]
     [SerializeField] [Range(0, 1)] private float _age;
-    [SerializeField] [Range(0, 1)] private float _maxLength;
+    [SerializeField] [Range(0, 2)] private float _maxLength;
     [SerializeField] [Range(0, 1)] private float _diameter;
+    [SerializeField] [Range(0, 1)] private float _flowerVariance;
+    [SerializeField] [Range(0, 1)] private float _leafVariance;
     [SerializeField] private float _angle;
-    [SerializeField] private float _yaw;
-    [SerializeField] private float _pitch;
-    [SerializeField] private float _roll;
 
     [Header("Tree Parts")]
-    [SerializeField] private GameObject _treeParent;
+    [SerializeField] private GameObject _treeParentBranch;
+    [SerializeField] private GameObject _treeParentLeaf;
+    [SerializeField] private GameObject _treeParentFlower;
     [SerializeField] private GameObject _treeBranch;
     [SerializeField] private GameObject _treeLeaf;
     [SerializeField] private GameObject _treeFlower;
-    public ProBuilderMesh Quad;
 
     [Header("Rules")]
     [SerializeField] private string _initialState;
@@ -73,7 +75,6 @@ public class TreeLSystem3D : MonoBehaviour
 
     void Generate(int _updateIteration)
     {
-
         // _tree = _parent;
         _currentString = _axiom;
 
@@ -108,16 +109,23 @@ public class TreeLSystem3D : MonoBehaviour
                 case '-':       // Turn right by angle Delta, Using rotation matrix R_U(-Delta).
                     TurnRight();
                     break;
-                case 'x':       // Pitch by angle Delta, Using rotation matrix R_L(-Delta).
-                    Pitch();
+                case '^':       // Pitch by angle Delta, Using rotation matrix R_L(-Delta).
+                    PitchUp();
                     break;
-                case 'y':
-                    Yaw();
+                case '&':
+                    PitchDown();
                     break;
-                case 'z':       // Roll right by angle Delta, Using rotation matrix R_H(-Delta).
-                    Roll();
+                case 'l':
+                    RollLeft();
                     break;
-                case '|':       // Turn around, Using rotation matrix R_H(180).
+                case '/':
+                    RollRight();
+                    break;
+                case 'h':
+                    Flower();
+                    break;
+                case 'a':
+                    Leaf();
                     break;
                 case '[':       // Push the current state 
                     _transformStack.Push(new TransformInfo3D()
@@ -138,6 +146,7 @@ public class TreeLSystem3D : MonoBehaviour
     }
     Quaternion _initialRotation;
     GameObject _branch;
+    Quaternion _rotation;
     void CreateBranch() //Create the tree branch
     {
         _branch = Instantiate(_treeBranch, transform.position, transform.rotation);              //Instantiate the branch at (0,0 + _posOffset,0)
@@ -149,46 +158,190 @@ public class TreeLSystem3D : MonoBehaviour
 
         transform.Translate(Vector3.up * _maxLength);
 
-        _branch.transform.parent = _treeParent.transform;
+        _branch.transform.parent = _treeParentBranch.transform;
 
-        _maxLength -= 0.1f;
+        _maxLength -= UnityEngine.Random.Range(0, 0.5f);
         if(_maxLength <= 0)
         {
-            _maxLength = 1;
+            _maxLength = 2;
         }
         //Debug.Log("Position: " + _branch.transform.position + " Scale: " + _branch.transform.localScale);
     }
+    void Flower()
+    {
+        float _rand = UnityEngine.Random.Range(0, _flowerVariance);
+        GameObject _flower = Instantiate(_treeFlower, transform.position, transform.rotation);
+        _flower.transform.localScale = new Vector3(_rand, _rand, _rand);
+        _flower.transform.parent = _treeParentFlower.transform;
+    }
+    void Leaf()
+    {
+        float _rand = UnityEngine.Random.Range(0, _leafVariance);
+        GameObject _leaf = Instantiate(_treeLeaf, transform.position, transform.rotation);
+        _leaf.transform.localScale = new Vector3(_rand, _rand, _rand);  
+        _leaf.transform.parent = _treeParentLeaf.transform;
+    }
+
     void ResetRotation()   //Move the postion upwards without instantiating a branch
     {
         float _random = UnityEngine.Random.Range(-90, 90);
         transform.rotation = Quaternion.Euler(0f, 0f, _random);
     }
-    void TurnLeft()     // +
+
+    /// <summary> 3x3 RotationUp
+    ///   Row             0             1          2    Column 
+    ///             |  Cos(angle) ,  Sin(angle) ,  0  |   0
+    /// Ru(angle) = | -Sin(angle) ,  Cos(angle) ,  0  |   1
+    ///             |     0       ,     0       ,  1  |   2
+    /// </summary>
+    /// <param name="angle">Rotation 3x3</param>
+    /// <returns>Gets the 3x3 rotation</returns>
+    Matrix4x4 RotationUp(float angle)
     {
-        float _random = UnityEngine.Random.Range(0, _angle);
-        transform.Rotate(Vector3.right * _random);
-    }
-    void TurnRight()    // -
+        Matrix4x4 _rotationUp = Matrix4x4.zero;
+
+        //First row
+        _rotationUp[0, 0] = Mathf.Cos(angle);
+        _rotationUp[0, 1] = Mathf.Sin(angle);
+        _rotationUp[0, 2] = 0f;
+        _rotationUp[0, 3] = 0f;
+        //Second row
+        _rotationUp[1, 0] = -Mathf.Sin(angle);
+        _rotationUp[1, 1] = Mathf.Cos(angle);
+        _rotationUp[1, 2] = 0f;
+        _rotationUp[1, 3] = 0f;
+        //Third row
+        _rotationUp[2, 0] = 0f;
+        _rotationUp[2, 1] = 0f;
+        _rotationUp[2, 2] = 1;
+        _rotationUp[2, 3] = 0f;
+        // Fourth row
+        _rotationUp[3, 0] = 0f;
+        _rotationUp[3, 1] = 0f;
+        _rotationUp[3, 2] = 0f;
+        _rotationUp[3, 3] = 1f;
+        return _rotationUp;
+    } 
+
+    /// <summary> 3x3 RotationPitch
+    ///     Row         0        1             2         Column  
+    ///             |   1   ,    0       ,     0       |   0
+    /// Rl(angle) = |   0   , Cos(angle) , -Sin(angle) |   1
+    ///             |   0   , Sin(angle) ,  Cos(angle) |   2
+    /// </summary>
+    /// <param name="angle">Parameter value to pass</param>
+    /// <returns></returns>
+    Matrix4x4 RotationPitch(float angle)
     {
-        float _random = UnityEngine.Random.Range(0, _angle);
-        transform.Rotate(Vector3.left * _random);
+        Matrix4x4 _rotationPitch = Matrix4x4.zero;
+
+        //First row
+        _rotationPitch[0, 0] = 1f;
+        _rotationPitch[0, 1] = 0f;
+        _rotationPitch[0, 2] = 0f;
+        _rotationPitch[0, 3] = 0f;
+        //Second row
+        _rotationPitch[1, 0] = 0f;
+        _rotationPitch[1, 1] = Mathf.Cos(angle);
+        _rotationPitch[1, 2] = -Mathf.Sin(angle);
+        _rotationPitch[1, 3] = 0f;
+        //Third row
+        _rotationPitch[2, 0] = 0f;
+        _rotationPitch[2, 1] = Mathf.Sin(angle);
+        _rotationPitch[2, 2] = Mathf.Cos(angle);
+        _rotationPitch[2, 3] = 0f;
+        // Fourth row
+        _rotationPitch[3, 0] = 0f;
+        _rotationPitch[3, 1] = 0f;
+        _rotationPitch[3, 2] = 0f;
+        _rotationPitch[3, 3] = 1f;
+
+        return _rotationPitch;
     }
-    void Pitch()    // x
+    /// <summary>
+    /// 3x3 RotationRoll
+    ///     Row          0          1        2          Column
+    ///             | Cos(angle) ,  0  , -Sin(angle) |    0
+    /// Rh(angle) = |    0       ,  1  ,     0       |    1
+    ///             | Sin(angle) ,  0  ,  Cos(angle) |    2
+    /// </summary>
+    /// <param name="angle"></param>
+    /// <returns></returns>
+    Matrix4x4 RotationHeading(float angle)
     {
-        float _random = UnityEngine.Random.Range(-_pitch, _pitch);
-        transform.rotation = Quaternion.Euler(_random, 0, 0);
-        //transform.Rotate(Vector3.right * _pitch);
+        Matrix4x4 _rotationHeading = Matrix4x4.zero;
+
+        //First row
+        _rotationHeading[0, 0] = Mathf.Cos(angle);
+        _rotationHeading[0, 1] = 0f;
+        _rotationHeading[0, 2] = -Mathf.Sin(angle);
+        _rotationHeading[0, 3] = 0f;
+        //Second row
+        _rotationHeading[1, 0] = 0f;
+        _rotationHeading[1, 1] = 1f;
+        _rotationHeading[1, 2] = 0f;
+        _rotationHeading[1, 3] = 0f;
+        //Third row
+        _rotationHeading[2, 0] = Mathf.Sin(angle);
+        _rotationHeading[2, 1] = 0f;
+        _rotationHeading[2, 2] = Mathf.Cos(angle);
+        _rotationHeading[2, 3] = 0f;
+        // Fourth row
+        _rotationHeading[3, 0] = 0f;
+        _rotationHeading[3, 1] = 0f;
+        _rotationHeading[3, 2] = 0f;
+        _rotationHeading[3, 3] = 1f;
+        return _rotationHeading;
     }
-    void Yaw()      // y
+    void TurnLeft()                     // +
     {
-        float _random = UnityEngine.Random.Range(-_yaw, _yaw);
-        transform.rotation = Quaternion.Euler(0, _random, 0);
-        //transform.Rotate(Vector3.up * _yaw);
+        Quaternion _currentRotation = transform.rotation;
+        Matrix4x4 _rotationUp = Matrix4x4.TRS(Vector3.zero, _currentRotation, Vector3.one) * RotationUp(_angle);
+        Quaternion _rotation = Quaternion.LookRotation(_rotationUp.GetColumn(2), _rotationUp.GetColumn(1));
+
+        transform.rotation = _rotation;
     }
-    void Roll()     // z
+    void TurnRight()                    // -
     {
-        float _random = UnityEngine.Random.Range(-_roll, _roll);
-        transform.rotation = Quaternion.Euler(0, 0, _random);
-        //transform.Rotate(Vector3.forward * _roll);
+        Quaternion _currentRotation = transform.rotation;
+        Matrix4x4 _rotationDown = Matrix4x4.TRS(Vector3.zero, _currentRotation, Vector3.one) * RotationUp(-_angle);
+        Quaternion _rotation = Quaternion.LookRotation(_rotationDown.GetColumn(2), _rotationDown.GetColumn(1));
+
+        transform.rotation = _rotation;
     }
+
+    void PitchUp()                      // ^
+    {
+        Quaternion _currentRotation = transform.rotation;
+        Matrix4x4 _pitchUp =  Matrix4x4.TRS(Vector3.zero, _currentRotation, Vector3.one) * RotationPitch(_angle);
+        Quaternion _rotation = Quaternion.LookRotation(_pitchUp.GetColumn(0), _pitchUp.GetColumn(1));
+
+        transform.rotation = _rotation;
+    }
+    void PitchDown()                    // &
+    {
+        Quaternion _currentRotation = transform.rotation;
+        Matrix4x4 _pitchDown = Matrix4x4.TRS(Vector3.zero, _currentRotation, Vector3.one) * RotationPitch(-_angle);
+        Quaternion _rotation = Quaternion.LookRotation(_pitchDown.GetColumn(0), _pitchDown.GetColumn(1));
+
+        transform.rotation = _rotation;
+    }
+    void RollLeft()                     // l
+    {
+        Quaternion _currentRotation = transform.rotation;
+        Matrix4x4 _rollLeft = Matrix4x4.TRS(Vector3.zero, _currentRotation, Vector3.one) * RotationHeading(_angle);
+        Quaternion _rotation = Quaternion.LookRotation(_rollLeft.GetColumn(0), _rollLeft.GetColumn(1));
+
+        transform.rotation = _rotation;
+    }
+
+    void RollRight()                    // /
+    {
+        Quaternion _currentRotation = transform.rotation;
+        Matrix4x4 _rollRight = Matrix4x4.TRS(Vector3.zero, _currentRotation, Vector3.one)* RotationHeading(-_angle);
+        Quaternion _rotation = Quaternion.LookRotation(_rollRight.GetColumn(0), _rollRight.GetColumn(1));
+
+        transform.rotation = _rotation;
+    }
+
 }
