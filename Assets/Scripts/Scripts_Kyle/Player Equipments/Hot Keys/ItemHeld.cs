3db792ac43e52a6ff3a70804e0234ff097c1fc7e
Tatanagi <kayrafaelchester1925@gmail.com>
2024-03-08 07:02:24 +0800

    Added a hot keys and item switch scroll wheel

---
+++ Assets/Scripts/Scripts_Kyle/Player Equipments/Hot Keys/ItemHeld.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.UI;
+
+public class ItemHeld : MonoBehaviour
+{
+    public GameObject pitchfork, slingshot, water_sprinkler;
+    public Scrollbar scrollbar;
+
+    private KeyCode[] _keyCodes =
+    {
+        KeyCode.Alpha1,
+        KeyCode.Alpha2,
+        KeyCode.Alpha3
+    };
+
+    private void Update()
+    {
+        // Handle item selection via number keys
+        if (Input.GetKeyDown(_keyCodes[0]))
+        {
+            SetActiveItem(pitchfork);
+        }
+        else if (Input.GetKeyDown(_keyCodes[1]))
+        {
+            SetActiveItem(slingshot);
+        }
+        else if (Input.GetKeyDown(_keyCodes[2]))
+        {
+            SetActiveItem(water_sprinkler);
+        }
+
+        // Handle item selection via mouse scroll wheel
+        float scroll = Input.GetAxisRaw("Mouse ScrollWheel");
+        if (scroll != 0f)
+        {
+            if (scroll > 0f)
+            {
+                // Scroll up
+                ScrollUp();
+            }
+            else
+            {
+                // Scroll down
+                ScrollDown();
+            }
+        }
+    }
+
+    private void SetActiveItem(GameObject item)
+    {
+        pitchfork.SetActive(item == pitchfork);
+        slingshot.SetActive(item == slingshot);
+        water_sprinkler.SetActive(item == water_sprinkler);
+    }
+
+    private void ScrollUp()
+    {
+        int activeIndex = GetActiveIndex();
+        if (activeIndex < 2)
+        {
+            activeIndex++;
+            SetActiveItem(GetItemAtIndex(activeIndex));
+        }
+    }
+
+    private void ScrollDown()
+    {
+        int activeIndex = GetActiveIndex();
+        if (activeIndex > 0)
+        {
+            activeIndex--;
+            SetActiveItem(GetItemAtIndex(activeIndex));
+        }
+    }
+
+    private int GetActiveIndex()
+    {
+        if (pitchfork.activeSelf)
+            return 0;
+        if (slingshot.activeSelf)
+            return 1;
+        if (water_sprinkler.activeSelf)
+            return 2;
+        return -1; // None active
+    }
+
+    private GameObject GetItemAtIndex(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                return pitchfork;
+            case 1:
+                return slingshot;
+            case 2:
+                return water_sprinkler;
+            default:
+                return null;
+        }
+    }
+}
