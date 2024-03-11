4673b635efc12f2958aeebbbc3fb0c0b82563994
CIIT601PC14\Student <max.sandejas@gmail.com>
2024-03-11 17:23:03 +0800

    made a mini enemy stats tracker; di pala naadd yung temp. enemy stats ko sa scriptable object so i added na (bulok ng ciit pc yoko na)

---
+++ Assets/EnemyStatsTracker.cs
@@ -0,0 +1,13 @@
+/*
+ Enemy Stats Tracker
+
+    Name        HEALTH      ATK DMG     ATK TIME        ATK RANGE       ATK DISTANCE        ATK COUNT       MOVE SPD        ANGL. SPD       ACCELERATION
+
+Giant Ant         10            10          10              10              10                  10              10              10              10
+
+Tree Cutter       10            10          10              10              10                  10              10              10              10
+
+Porcupine         10            10          10              10              10                  10              10              10              10
+
+Caterpillar       10            10          10              10              10                  10              10              10              10
+ */
