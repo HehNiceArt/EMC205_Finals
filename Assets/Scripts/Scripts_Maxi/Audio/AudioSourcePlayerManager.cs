a2233b7a5893e4020d5f6582d7330a902e36fc82
CIIT601PC14\Student <max.sandejas@gmail.com>
2024-03-11 18:17:10 +0800

    monkey code audio w/o headphones

---
+++ Assets/Scripts/Scripts_Maxi/Audio/AudioSourcePlayerManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AudioSourcePlayerManager : MonoBehaviour
+{
+    public AudioClip mainMenuBGM;
+    public AudioClip gameBGM;
+
+    private static AudioSourcePlayerManager instance = null;
+    private new AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        PlaySceneBGM();
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        if (instance == this) return;
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for scene changes
+        if (SceneManager.GetActiveScene().name == "MainMenu" && audio.clip != mainMenuBGM)
+        {
+            audio.clip = mainMenuBGM;
+            PlaySceneBGM();
+        }
+
+        else if (SceneManager.GetActiveScene().name == "MASTER_MainLevel" && audio.clip != gameBGM)
+        {
+            audio.clip = gameBGM;
+            PlaySceneBGM();
+        }
+    }
+
+    void PlaySceneBGM()
+    {
+        audio.Stop();
+        audio.Play();
+    }
+}
